d4d2842b614b42095f656be0986201bd0d33d987
vennby <venn.vallabhaneni@gmail.com>
2026-06-28 18:35:12 +0400

    residuals-removal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { AnonAadhaarClientProvider } from "../components/providers";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <AnonAadhaarClientProvider>{children}</AnonAadhaarClientProvider>
-      </body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
