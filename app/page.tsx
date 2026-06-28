eee8b86185b759cb96d0e98edba4591bfec16c3d
vennby <venn.vallabhaneni@gmail.com>
2026-06-28 18:32:54 +0400

    aadhar-fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import CreateListing from "@/components/CreateListing";
-import AadhaarLogin from "../components/AadharLogin";
 
 export default function Home() {
   const router = useRouter();
